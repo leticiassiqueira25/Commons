11eea18d9cfab12801edad0a226e706d12b15fe6
leticiassiqueira25 <leticiassiqueira25@gmail.com>
2021-01-26 10:00:21 -0300

    Initial commit

---
+++ Example/CommonsService/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CommonsService
+//
+//  Created by leticiassiqueira25 on 01/26/2021.
+//  Copyright (c) 2021 leticiassiqueira25. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
